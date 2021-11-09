52eab6f85d77be18920b8d5f340b3bc18070a7ae
Claudius Kienle <privat@claudiuskienle.de>
2021-11-09 17:44:37 +0100

    started zivid frame conversions

--- src/models/camera_interfaces/Zivid.py
+++ src/models/camera_interfaces/Zivid.py
@@ -1,5 +1,6 @@
 import zivid
 import datetime
+import cv2
 
 class Zivid:
 
@@ -11,7 +12,6 @@ class Zivid:
 
         print("Configuring settings")
         self.settings = zivid.Settings()
-        self.settings.experimental.engine = "phase"
         self.settings.acquisitions.append(zivid.Settings.Acquisition())
         self.settings.acquisitions[0].aperture = 5.66
         self.settings.acquisitions[0].exposure_time = datetime.timedelta(microseconds=6500)
@@ -24,3 +24,18 @@ class Zivid:
             data_file = "Frame.zdf"
             print(f"Saving frame to file: {data_file}")
             frame.save(data_file)
+
+            rgb_image = Zivid._convert_2_2d(point_cloud=frame)
+
+            return rgb_image
+
+
+    def _convert_2_2d(point_cloud):
+        """Convert from point cloud to 2D image.
+        Args:
+            point_cloud: a handle to point cloud in the GPU memory
+        Returns rgb opencv image 
+        """
+        rgba = point_cloud.copy_data("rgba")
+        bgr = cv2.cvtColor(rgba, cv2.COLOR_RGBA2BGR)
+        return bgr
