9b151fac4adbe3ef703381339c5bf7edd49740cb
Claudius Kienle <privat@claudiuskienle.de>
2022-01-23 16:30:10 +0100

    update wandb comands

--- src/trainers/trainer.py
+++ src/trainers/trainer.py
@@ -110,16 +110,17 @@ class OutOfFoldTrainer:
 
         # (Initialize logging)
         if activate_wandb:
-            experiment = wandb.init(
-                project=net.name, resume='allow', entity="depth-denoising")
-            experiment.config.update(dict(epochs=epochs, batch_size=batch_size,
-                                          learning_rate=learning_rate, save_checkpoint=save_checkpoint, img_scale=img_scale,
-                                          amp=amp))
+            experiment = wandb.init(project=net.name, resume='allow', entity="depth-denoising", reinit=True)
+            experiment.config.update(
+                dict(epochs=epochs, batch_size=batch_size,
+                     learning_rate=learning_rate, save_checkpoint=save_checkpoint, img_scale=img_scale,
+                     amp=amp
+            )
 
         net.to(device)
 
-        n_train = len(train_set)
-        n_val = len(val_set)
+        n_train=len(train_set)
+        n_val=len(val_set)
 
         logging.info(f'''Starting training:
             Epochs:          {epochs}
@@ -133,17 +134,17 @@ class OutOfFoldTrainer:
             Mixed Precision: {amp}
             ''')
 
-        loader_args = dict(
+        loader_args=dict(
             batch_size=batch_size,
             num_workers=4,
             pin_memory=True
         )
-        train_loader = DataLoader(
+        train_loader=DataLoader(
             train_set,
             shuffle=True,
             **loader_args
         )
-        val_loader = DataLoader(
+        val_loader=DataLoader(
             val_set,
             shuffle=False,
             drop_last=True,
@@ -151,41 +152,41 @@ class OutOfFoldTrainer:
         )
 
         # Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-        optimizer = optim.RMSprop(
+        optimizer=optim.RMSprop(
             net.parameters(),
             lr=learning_rate,
             weight_decay=1e-8,
             momentum=0.9
         )
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        scheduler=optim.lr_scheduler.ReduceLROnPlateau(
             optimizer,
             'max',
             patience=2
         )
-        grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
-        criterion = nn.L1Loss()
-        global_step = 0
+        grad_scaler=torch.cuda.amp.GradScaler(enabled=amp)
+        criterion=nn.L1Loss()
+        global_step=0
 
         # Begin training
         for epoch in range(epochs):
             net.train()
-            epoch_loss = 0
+            epoch_loss=0
             with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
                 for batch in train_loader:
-                    images = batch['image']
-                    true_masks = batch['mask']
+                    images=batch['image']
+                    true_masks=batch['mask']
 
                     assert images.shape[1] == net.n_channels, \
                         f'Network has been defined with {net.n_channels} input channels, ' \
                         f'but loaded images have {images.shape[1]} channels. Please check that ' \
                         'the images are loaded correctly.'
 
-                    images = images.to(device=device, dtype=torch.float32)
-                    true_masks = true_masks.to(device=device, dtype=torch.float32)
+                    images=images.to(device=device, dtype=torch.float32)
+                    true_masks=true_masks.to(device=device, dtype=torch.float32)
 
                     with torch.cuda.amp.autocast(enabled=amp):
-                        masks_pred = net(images)
-                        loss = criterion(masks_pred, true_masks)
+                        masks_pred=net(images)
+                        loss=criterion(masks_pred, true_masks)
 
                     optimizer.zero_grad(set_to_none=True)
                     grad_scaler.scale(loss).backward()
@@ -206,21 +207,21 @@ class OutOfFoldTrainer:
                     pbar.set_postfix(**{'loss (batch)': loss.item()})
 
                     # Evaluation round
-                    division_step = (n_train // (10 * batch_size))
+                    division_step=(n_train // (10 * batch_size))
                     if division_step > 0 and global_step % division_step == 0:
-                        val_loss = self.evaluate(net, val_loader, device)
+                        val_loss=self.evaluate(net, val_loader, device)
                         scheduler.step(val_loss)
 
                         logging.info('Validation Loss: {}'.format(val_loss))
 
                         if activate_wandb:
-                            histograms = {}
+                            histograms={}
                             for tag, value in net.named_parameters():
-                                tag = tag.replace('/', '.')
+                                tag=tag.replace('/', '.')
                                 histograms['Weights/' +
-                                           tag] = wandb.Histogram(value.data.cpu())
+                                           tag]=wandb.Histogram(value.data.cpu())
                                 histograms['Gradients/' +
-                                           tag] = wandb.Histogram(value.grad.data.cpu())
+                                           tag]=wandb.Histogram(value.grad.data.cpu())
 
                             experiment.log({
                                 'learning rate': optimizer.param_groups[0]['lr'],
@@ -244,10 +245,13 @@ class OutOfFoldTrainer:
                            'checkpoint_epoch{}.pth'.format(epoch + 1)))
                 logging.info(f'Checkpoint {epoch + 1} saved!')
 
+        if activate_wandb:
+            wandb.finish()
+
 
 def main(args):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    oof = OutOfFoldTrainer(
+    device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    oof=OutOfFoldTrainer(
         device=device,
         dataset_path=args.dataset_path,
         scale=args.scale_images,
@@ -257,7 +261,7 @@ def main(args):
         bilinear=args.bilinear
     )
 
-    params = dict(
+    params=dict(
         device=device,
         epochs=args.epochs,
         batch_size=args.batch_size,
@@ -294,9 +298,9 @@ def main(args):
 
 
 if __name__ == '__main__':
-    file_dir = Path(__file__).parent
+    file_dir=Path(__file__).parent
 
-    parser = ArgumentParser()
+    parser=ArgumentParser()
     parser.add_argument("--epochs", type=int, default=5)  # Number of epochs
     parser.add_argument("--batch_size", type=int, default=1)  # Batch size
     parser.add_argument("--learning_rate", type=float, default=0.00001)  # Learning rate
@@ -305,12 +309,16 @@ if __name__ == '__main__':
     parser.add_argument("--validation_percentage", type=float, default=10.0)
     # Percent of the data that is used as validation (0-100)
     parser.add_argument("--oof_p", type=float, default=1/3)  # length of each P for OOF training)
-    parser.add_argument("--wandb", type=lambda x:bool(strtobool(x)), nargs='?', const=True, default=True)  # toggle the usage of wandb for logging purposes
-    parser.add_argument("--save", type=lambda x: bool(strtobool(x)), nargs='?', const=True, default=True)   # save trained model
+    parser.add_argument("--wandb", type=lambda x: bool(strtobool(x)), nargs='?', const=True,
+                        default=True)  # toggle the usage of wandb for logging purposes
+    parser.add_argument("--save", type=lambda x: bool(strtobool(x)), nargs='?',
+                        const=True, default=True)   # save trained model
     parser.add_argument("--dataset_path", type=Path, default=file_dir / "../../resources/images/calibrated")
     parser.add_argument("--dir_checkpoint", type=Path, default=file_dir / "../../resources/networks")
-    parser.add_argument("--bilinear", type=lambda x:bool(strtobool(x)), nargs='?', const=True, default=True)      # unet using bilinear
+    parser.add_argument("--bilinear", type=lambda x: bool(strtobool(x)), nargs='?',
+                        const=True, default=True)      # unet using bilinear
     parser.add_argument("--n_input_channels", type=int, default=4)          # rgbd as input
     parser.add_argument("--n_output_channels", type=int, default=1)     # depth as output
-    parser.add_argument("--amp", type=lambda x:bool(strtobool(x)), nargs='?', const=True, default=False)  # Use mixed precision
+    parser.add_argument("--amp", type=lambda x: bool(strtobool(x)), nargs='?',
+                        const=True, default=False)  # Use mixed precision
     main(parser.parse_args())
