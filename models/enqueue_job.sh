9237db249126146625046cc46bfc43271b7f7e48
ng3916@partner.kit.edu <ng3916@uc2n997.localdomain>
2022-01-25 15:55:29 +0100

    bwunicluster train files added

---
+++ models/enqueue_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu_4
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=6:00:00
+#SBATCH --mem=20000mb   
+#SBATCH --gres=gpu:2
+#SBATCH --export=ALL,EXECUTABLE=./train_models.sh
+#SBATCH --output="train_models.out"
+#SBATCH -J OpenMP_Test
+
+#Usually you should set
+export KMP_AFFINITY=compact,1,0
+#export KMP_AFFINITY=verbose,compact,1,0 prints messages concerning the supported affinity
+#KMP_AFFINITY Description: https://software.intel.com/en-us/node/524790#KMP_AFFINITY_ENVIRONMENT_VARIABLE
+
+export OMP_NUM_THREADS=$((${SLURM_JOB_CPUS_PER_NODE}/2))
+echo "Executable ${EXECUTABLE} running on ${SLURM_JOB_CPUS_PER_NODE} cores with ${OMP_NUM_THREADS} threads"
+startexe=${EXECUTABLE}
+echo $startexe
+exec $startexe
---
+++ models/train_models.sh
@@ -0,0 +1,1 @@
+python $(dirname $0)/../src/trainers/trainer.py --epochs=500 --batch_size=5 --save=True
