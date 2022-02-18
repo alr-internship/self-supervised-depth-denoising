7e1803e7393baf7fa45040f20d0e157143c93a89
Claudius Kienle <privat@claudiuskienle.de>
2022-02-18 23:50:28 +0100

    updated temporary script

--- src/data_processing/compute_depth_bounds.py
+++ src/data_processing/compute_depth_bounds.py
@@ -5,30 +5,44 @@ from matplotlib import pyplot as plt
 
 import numpy as np
 from tqdm import tqdm
+from joblib import Parallel, cpu_count, delayed
 
 from dataset.dataset_interface import DatasetInterface
+from utils.general_utils import split
 
 
-def main(args):
-    files = DatasetInterface.get_paths_in_dir(args.dir)
+def compute_bounds(files):
     min_depth = np.inf
     max_depth = -np.inf
     for file in tqdm(files, desc='computing depth bounds for normalization'):
         _, rs_depth, _, zv_depth, _ = DatasetInterface.load(file)
 
-        plt.imshow(rs_depth)
-        plt.imshow(zv_depth)
-        plt.show()
+        print(np.nanmin(zv_depth))
+        print(np.nanmin(np.where(zv_depth == 0, np.nan, zv_depth)))
         min_t = np.nanmin([rs_depth, zv_depth])
         max_t = np.nanmax([rs_depth, zv_depth])
-        print(min_t, max_t)
         min_depth = min(min_t, min_depth)
         max_depth = max(max_t, max_depth)
+        print(min_depth, max_depth)
+    return min_depth, max_depth
+
+def main(args):
+    files = DatasetInterface.get_paths_in_dir(args.dir)
+    jobs = args.jobs
+
+    files_chunked = split(files, jobs)
+    min_max_depth_list = Parallel(n_jobs=jobs)(
+        delayed(compute_bounds)(files_chunk)
+        for files_chunk in files_chunked
+    )
+
+    min_depth = np.min(min_max_depth_list, axis=0)[0]
+    max_depth = np.max(min_max_depth_list, axis=0)[1]
     print(f"computed normalization bounds: min {min_depth}, max {max_depth}")
 
 
 if __name__ == "__main__":
     argparse = ArgumentParser()
-    argparse.add_argument("dir", type=Path, 
-    help="dataset directory the bounds should computed for")
+    argparse.add_argument("dir", type=Path, help="dataset directory the bounds should computed for")
+    argparse.add_argument("--jobs", type=int, default=cpu_count())
     main(argparse.parse_args())
