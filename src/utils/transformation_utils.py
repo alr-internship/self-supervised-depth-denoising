41d79c039ba4dab8a3b5eb282d2609609b1ea8c9
dapetri <dapetri0@gmail.com>
2022-03-01 13:19:14 +0100

    adding visualisation for old vs new method

--- src/utils/transformation_utils.py
+++ src/utils/transformation_utils.py
@@ -44,6 +44,38 @@ def fill_to_shape(array, shape, fill_value, dtype):
     array_large[:s[0], :s[1]] = array[:s[0], :s[1]]
     return array_large
 
+def pcd_to_imgs_old(pcd, ci: dict, depth_scale: float = 1000.0):
+    points = np.asarray(pcd.points)
+    colors = np.asarray(pcd.colors)
+
+    # camera parameters
+    f = np.array([ci['fx'], ci['fy']])
+    c = np.array([ci['cx'], ci['cy']])
+
+    # convert to 3d to 2d space
+    depths = points[:, 2]
+    pixels = points[:, :2] * f / np.expand_dims(depths, axis=1) + c
+    pixels = np.round(pixels).astype(np.uint16)
+    
+    # create empty frames for final rgb and depth images
+    ul_corner = np.min(pixels, axis=0)
+    lr_corner = np.max(pixels, axis=0)
+    picture_size = np.round_(lr_corner).astype(np.uint16).T
+    rgb_frame = np.zeros((picture_size[1] + 1, picture_size[0] + 1, 3), dtype=np.uint8)
+    # depth frame filled with NaNs
+    depth_frame = np.empty((picture_size[1] + 1, picture_size[0] + 1), dtype=np.float32)
+    depth_frame[:] = np.nan
+
+    # fill respective pixels with depth and rgb info
+    pixels = pixels.T
+    pixels = (pixels[1], pixels[0])
+    rgb_frame[pixels] = colors * 255
+    depth_frame[pixels] = depths * depth_scale
+
+    bgr_frame = to_bgr(rgb_frame)
+
+    return bgr_frame, depth_frame, ul_corner, lr_corner
+
 
 def pcd_to_imgs(pcd, ci: dict, depth_scale: float = 1000.0):
     points = np.asarray(pcd.points)
@@ -72,6 +104,8 @@ def pcd_to_imgs(pcd, ci: dict, depth_scale: float = 1000.0):
         if unique_max_depths[inv[i]] > depths[i]:
             unique_max_depths[inv[i]] = depths[i]
     depths = unique_max_depths[inv]
+    
+    # print(np.isinf(depths).any())
 
     # create empty frames for final rgb and depth images
     ul_corner = np.min(pixels, axis=0)
